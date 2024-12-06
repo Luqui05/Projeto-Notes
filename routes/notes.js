1ffe95e62890d77bdb2ea203ca9be60b51fb4504
LucasTeixeira-sudo <lucashtribeiro@hotmail.com>
2024-12-06 15:52:59 -0300

    feat: View edição de notas

--- routes/notes.js
+++ routes/notes.js
@@ -39,4 +39,13 @@ router.get("/detail/:id", async function (req, res) {
   res.render("notes/detail", { note });
 });
 
+//View de edição de notas
+router.get("/edit/:id", async function (req, res) {
+  const id = ObjectId.createFromHexString(req.params.id);
+
+  const note = await db.getDb().db().collection("notes").findOne({ _id: id });
+
+  res.render("notes/edit", { note });
+});
+
 module.exports = router;
