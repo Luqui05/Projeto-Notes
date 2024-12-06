42461ef84f4b7896b06aa4441d5a711b8ea365b1
LucasTeixeira-sudo <lucashtribeiro@hotmail.com>
2024-12-06 14:11:31 -0300

    feat: Inserindo notas no banco de dados

--- routes/notes.js
+++ routes/notes.js
@@ -1,9 +1,23 @@
-const Router = require('express').Router;
+const Router = require("express").Router;
+const db = require("../db/connection");
+const { ObjectId } = require("mongodb");
 
 const router = Router();
 
-router.get('/', function (req, res) {
-  res.render('notes/create');
-})
+// Form criação de nota
+router.get("/", function (req, res) {
+  res.render("notes/create");
+});
+
+// Envio da nota para inserção no banco
+router.post("/", function (req, res) {
+  const data = req.body;
+  const title = data.title;
+  const description = data.description;
+
+  db.getDb().db().collection("notes").insertOne({ title, description });
+
+  res.redirect(301, "/");
+});
 
 module.exports = router;
