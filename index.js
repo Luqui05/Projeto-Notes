44f3df5dabde4e3abaf0ec38531e5c528ba12f75
LucasTeixeira-sudo <lucashtribeiro@hotmail.com>
2024-12-06 13:46:13 -0300

    feat: View de criar notas

--- index.js
+++ index.js
@@ -14,10 +14,14 @@ app.engine('handlebars', exphbs.engine({
 app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 
+const notesRoutes = require('./routes/notes')
+
 // Rotas
 app.get('/', function (req, res) {
   res.render('home');
 })
+
+app.use('/notes', notesRoutes)
 
 app.listen(port, () => {
   console.log(`Projeto rodando na porta: ${port}`)
